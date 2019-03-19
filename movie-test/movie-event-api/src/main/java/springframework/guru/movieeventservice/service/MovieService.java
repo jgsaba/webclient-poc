5e92dc4b92e126319e1219c387f861c9781464c6
Jorge Saba <jgsaba3@gmail.com>
2019-03-19 12:08:35 -0300

    deleção de projetos nao relacionados

--- movie-test/movie-event-api/src/main/java/springframework/guru/movieeventservice/service/MovieService.java
+++
@@ -1,9 +0,0 @@
-package springframework.guru.movieeventservice.service;
-
-import springframework.guru.movieeventservice.domain.MovieEvent;
-import reactor.core.publisher.Flux;
-
-
-public interface MovieService {
-	Flux<MovieEvent> events();
-}
